8b041597dcdeb17b840fe9132385c9139ede844e
Mourad <mourad.elhadj.mimoune@akretion.com>
2023-11-20 18:37:55 +0100

    [14.0][IMP] l10n_fr_ecotaxe: manage multi ecotaxe classification by product

--- l10n_fr_ecotaxe/models/account_move_line.py
+++ l10n_fr_ecotaxe/models/account_move_line.py
@@ -8,22 +8,46 @@ from odoo import api, fields, models
 class AcountMoveLine(models.Model):
     _inherit = "account.move.line"
 
+    ecotaxe_line_ids = fields.One2many(
+        'account.move.line.ecotaxe',
+        "account_move_line_id",
+        string="Ecotaxe lines",
+        copy = True,
+    )
     subtotal_ecotaxe = fields.Float(store=True, compute="_compute_ecotaxe")
-    unit_ecotaxe_amount = fields.Float(
+    ecotaxe_amount_unit = fields.Float(
         string="Ecotaxe Unit.",
         store=True,
         compute="_compute_ecotaxe",
     )
 
-    @api.depends("move_id.currency_id", "product_id", "quantity")
+    @api.depends("move_id.currency_id", "ecotaxe_line_ids",
+                 "ecotaxe_line_ids.ecotaxe_amount_unit",
+                 "ecotaxe_line_ids.ecotaxe_amount_total")
     def _compute_ecotaxe(self):
         for line in self:
-            unit = line.product_id.ecotaxe_amount
+            unit = sum(line.ecotaxe_line_ids.mapped('ecotaxe_amount_unit'))
+            subtotal_ecotaxe = sum(line.ecotaxe_line_ids.mapped('ecotaxe_amount_total'))
+
             if line.move_id.currency_id:
                 unit = line.move_id.currency_id.round(unit)
+                subtotal_ecotaxe = line.move_id.currency_id.round(subtotal_ecotaxe)
             line.update(
                 {
-                    "unit_ecotaxe_amount": unit,
-                    "subtotal_ecotaxe": unit * line.quantity,
+                    "ecotaxe_amount_unit": unit,
+                    "subtotal_ecotaxe": subtotal_ecotaxe,
                 }
             )
+
+    @api.onchange('product_id')
+    def _onchange_product_ecotaxe_line(self):
+        """ Unlink and recreate ecotaxe_lines when modifying the product_id."""
+        if not self.product_id:
+            self.ecotaxe_line_ids = [(5,)]  # Remove all ecotaxe classification
+        else:
+            ecotax_cls_vals = []
+            for ecotax_cls in self.product_id.ecotaxe_classification_ids:
+                ecotax_cls_vals.append(
+                    (0, 0, {'ecotaxe_classification_id': ecotax_cls.id})
+                )
+            self.ecotaxe_line_ids = ecotax_cls_vals
--- l10n_fr_ecotaxe/models/product_template.py
+++ l10n_fr_ecotaxe/models/product_template.py
@@ -8,34 +8,37 @@ from odoo import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    ecotaxe_classification_id = fields.Many2one(
+
+    ecotaxe_classification_ids = fields.Many2many(
         "account.ecotaxe.classification",
-        string="Ecotaxe Classification",
+        'product_template_rel_ecotaxe_classif',
+        string="Ecotaxe Classifications",
     )
     ecotaxe_amount = fields.Monetary(
         compute="_compute_ecotaxe",
         help="Ecotaxe Amount computed form Classification",
         store=True,
     )
-    manual_fixed_ecotaxe = fields.Monetary(
-        help="Manual Fixed ecotaxe.\n"
+    force_ecotaxe_amount = fields.Monetary(
+        help="Force ecotaxe amount.\n"
         "Allow to subtite default Ecotaxe Classification\n"
     )
 
     @api.depends(
-        "ecotaxe_classification_id",
-        "ecotaxe_classification_id.ecotaxe_type",
-        "ecotaxe_classification_id.ecotaxe_coef",
+        "ecotaxe_classification_ids",
+        "ecotaxe_classification_ids.ecotaxe_type",
+        "ecotaxe_classification_ids.ecotaxe_coef",
         "weight",
-        "manual_fixed_ecotaxe",
+        "force_ecotaxe_amount",
     )
     def _compute_ecotaxe(self):
         for tmpl in self:
-            ecotax_cls = tmpl.ecotaxe_classification_id
-            if ecotax_cls.ecotaxe_type == "weight_based":
-                amt = ecotax_cls.ecotaxe_coef * (tmpl.weight or 0.0)
-            elif tmpl.manual_fixed_ecotaxe:
-                amt = tmpl.manual_fixed_ecotaxe
-            else:
-                amt = ecotax_cls.default_fixed_ecotaxe
+            amt = 0.0
+            for ecotax_cls in tmpl.ecotaxe_classification_ids:
+                if ecotax_cls.ecotaxe_type == "weight_based":
+                    amt += ecotax_cls.ecotaxe_coef * (tmpl.weight or 0.0)
+                elif tmpl.force_ecotaxe_amount:
+                    amt += tmpl.force_ecotaxe_amount
+                else:
+                    amt += ecotax_cls.default_fixed_ecotaxe
             tmpl.ecotaxe_amount = amt
